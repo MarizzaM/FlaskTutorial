e59c965ab51e0c1e34212505f127cc283355cf46
Pavlova Marina <marizza.mil89@gmail.com>
2022-02-18 21:16:22 +0200

    Flask_05_Sessions

--- tutorial4.py
+++ tutorial4.py
@@ -2,9 +2,11 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -14,9 +16,11 @@ def login():
     else:
         return render_template("login.html")
 
+
 @app.route("/<usr>")
 def user(usr):
     return f"<h1>{usr}</h1>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
